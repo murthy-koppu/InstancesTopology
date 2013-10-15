064130c3cda6eb4ff39bb7750f53931e9a61ac58
murthy-koppu <murthy.koppu@imaginea.com>
2013-10-16 02:23:18 +0530

    Added js file changes to correct

--- src/main/java/com/imaginea/serverlocator/retrials/TestMySQLLocator.java
+++
@@ -1,9 +0,0 @@
-package com.imaginea.serverlocator.retrials;
-import com.imaginea.serverlocator.factory.ServerLocatorFactory;
-
-
-public class TestMySQLLocator {
-public static void main(String[] args) {
-	ServerLocatorFactory.getServerLocator("localhost", 3306);
-}
-}
